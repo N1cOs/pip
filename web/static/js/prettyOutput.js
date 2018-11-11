7365e6be258202a0c3635a75065e18102b741475
N1cOs <N1cOs@users.noreply.github.com>
2018-11-12 01:21:25 +0300

    added pretty output of the history table

--- web/static/js/prettyOutput.js
+++ web/static/js/prettyOutput.js
@@ -1,11 +1,13 @@
 const results = document.querySelectorAll('.previous_checks td');
-for(var i = 0; i < results.length; i++){
-    if(results[i].innerHTML == "false"){
-        results[i].style.color = "red";
-        results[i].style.fontWeight = "bold";
+for(let i = 0; i < results.length; i++){
+    if(results[i].innerText === 'false'){
+        results[i].innerText = 'Промах';
+        results[i].style.color = 'red';
+        results[i].style.fontWeight = 'bold';
     }
-    else if (results[i].innerHTML == "true") {
-        results[i].style.color = "green";
-        results[i].style.fontWeight = "bold";
+    else if (results[i].innerText === 'true') {
+        results[i].innerText = 'Попадание';
+        results[i].style.color = 'green';
+        results[i].style.fontWeight = 'bold';
     }
 }
